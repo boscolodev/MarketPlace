c44f216a9d5e2cbce92c5371e28845b16bc74c89
boscolo.dev <boscolo.dev@gmail.com>
2022-10-04 12:36:01 -0300

    User Project Created

---
+++ mkt-user/src/main/java/dev/boscolo/mktuser/MktUserApplication.java
@@ -0,0 +1,13 @@
+package dev.boscolo.mktuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MktUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MktUserApplication.class, args);
+	}
+
+}
---
+++ mkt-user/src/test/java/dev/boscolo/mktuser/MktUserApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.boscolo.mktuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MktUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
